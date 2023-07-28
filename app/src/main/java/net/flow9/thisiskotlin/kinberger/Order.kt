7f7287f44dab3c26c4843033c015cf93481d064d
seongssu <loveforggong@gmail.com>
2023-07-28 11:08:05 +0900

    [FIX] : 🔧 내용수정 11:07 자잘한 내용

--- app/src/main/java/net/flow9/thisiskotlin/kinberger/Order.kt
+++ app/src/main/java/net/flow9/thisiskotlin/kinberger/Order.kt
@@ -2,27 +2,32 @@ package net.flow9.thisiskotlin.kinberger
 
 import kotlinx.coroutines.selects.select
 
-class Order: Item() {
-    override fun function(){
+class Order : Item() {
+    override fun function() {
         println("[ORDER MENU]")
         println("[1]order\t\t|\t장바구니를 확인 후 주문합니다.\n[2]Cancel\t\t|\t진행중인 주문을 취소합니다.")
         while (true) {
             try {
                 val x: String? = readLine()
                 val y = x?.toInt() ?: -1
-                if(y<=-1 || y>2){
+                if (y <= -1 || y > 2) {
                     println("다시 선택해주세요")
-                }else {
-                    when(y){
+                } else {
+                    when (y) {
                         1 -> {
                             println("아래와 같이 주문 하시겠습니까?\n[ Orders ]")
+                            for (common in Cart.menus) {
+                                if (!common.introduce.contains("아이스크림")) {
+                                    println("메뉴: ${common.name}가격: ${common.price}원\t\t설명: ${common.introduce}")
+                                }
+                            }
                             for ((menu, flavor) in ForzenCustard.selectedMenus) {
                                 println("메뉴: ${menu.name}가격: ${menu.price}원\t\t설명: ${menu.introduce}\t\t맛: ${flavor}")
                             }
                             println("[1]주문\t\t\t [2]메뉴판")
                             var add = 0
                             for (menuItem in Cart.menus) {
-                               add += menuItem.price
+                                add += menuItem.price
                             }
                             println("▶ 계산서 : 총 ${add}원 입니다.")
                             println("▶ 잔액${Cart.money}")
@@ -31,20 +36,22 @@ class Order: Item() {
                                 try {
                                     val a: String? = readLine()
                                     val b = a?.toInt() ?: -1
-                                    if(b<=0 || b>2){
+                                    if (b <= 0 || b > 2) {
                                         println("▶ 다시 선택해주세요")
-                                    }else {
-                                        if(Cart.money>=add){
-                                            when(b){
+                                    } else {
+                                        if (Cart.money >= add) {
+                                            when (b) {
                                                 1 -> {
                                                     println("▶ 잔액${Cart.money}원 중 ${add}원 사용하여 구매 완료 했습니다.")
-                                                    println("▶ 현재 잔액은 ${Cart.money-add}원 입니다.")
+                                                    println("▶ 현재 잔액은 ${Cart.money - add}원 입니다.")
                                                     Cart.addMenus1()
+
                                                     Play().run()
                                                 }
+
                                                 2 -> Play().run()
                                             }
-                                        }else {
+                                        } else {
                                             println("▶ 잔액이 부족합니다.")
                                             return Play().run()
                                         }
@@ -52,18 +59,19 @@ class Order: Item() {
                                     }
 
                                 } catch (e: Exception) {
-                                    println("다시 입력해주세요")
+                                    return Play().run()
                                 }
                             }
 
                             return
                         }
+
                         2 -> return Play().run()
                     }
                 }
 
             } catch (e: Exception) {
-                println("다시 입력해주세요")
+                return Play().run()
             }
         }
     }
